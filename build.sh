d54ce2f6a18ffd4a3e0806f871f5635486108e63
Moderocky <admin@moderocky.com>
2022-02-19 13:13:20 +0000

    Move license/readme copying to shell layer.

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 node build.js
 
+cp SAMPLE-README.md syntax-atom/README.md
+cp SAMPLE-README.md syntax-vscode/README.md
+cp SAMPLE-README.md syntax-st3/README.md
+echo "Updated README files."
+
+cp LICENSE syntax-atom/LICENSE.md
+cp LICENSE syntax-vscode/LICENSE.md
+cp LICENSE syntax-st3/LICENSE.md
+echo "Updated LICENSE files."
+
 cd syntax-vscode/
 
 rm ./*.vsix
